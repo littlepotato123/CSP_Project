b3752647167dfecc43475160d97c71f7c37b0372
littlepotato123 <sopanda0910@gmail.com>
2022-02-28 21:13:38 -0500

    Elevator Init

--- README.md
+++ README.md
@@ -3,12 +3,11 @@ Monopoly
 - List of Chance Cards
 - List of Community Chest Cards
 - Player position in Coordinates (2D Array) ([5][0])
-- Go To Jail
 - 4 Players Go
 - Roll Dice 
 - Money Management
 
-https://www.monopolyland.com/list-monopoly-chance-community-chest-cards/
+https://www.monopolyland.com/list-monopoly-chance-community-chest-cards
 
 ToDo
 - Buying Cards
@@ -24,3 +23,23 @@ Player Colors
 - Player 2: #35858B
 - Player 3: #4FBDBA
 - Player 4: #AEFEFF
+
+
+Elevator
+- List of all PINS
+- List of AUTHORIZED PINS
+  - 0-3 (ALL)
+  - 4-8
+  - 9-15
+  - 16 - 25
+- Elevator Logic
+- Elevator Keypad (ALL FLOORS)
+  - Enter PIN
+  - All Floor Numbers
+- Elevator Movement
+
+ToDo
+- Elevator Keypad
+- Elevator Movement
+- List of ALL PINS
+- List of AUTHORIZED PINS
--- elevator/src/App.tsx
+++ elevator/src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App: React.FC = () => {
+  const [top, setTop] = useState<number>(0);
+
+  return(
+    <div>
+      <div style={{ top:`${top}px` }} className="elevator">
+        Hi
+      </div>
+      <button onClick={() => {
+        setTop(100);
+      }}>
+        Click
+      </button>
     </div>
-  );
-}
+  )
+};
 
 export default App;
